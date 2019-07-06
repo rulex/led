a0e7313d3e1d977d2699ecce167b2fbf0c15c795
Lars Wikberg <lars.wikberg@nokia.com>
2019-07-06 21:38:14 +0300

    redir light stderr to /dev/null

--- led.sh
+++ led.sh
@@ -7,24 +7,24 @@ export LED_ON=0.5             # default led_blink ON time in seconds
 export LED_OFF=0.5            # default led_blink OFF time in seconds
 
 function led_set() {
-    light -L \
+    light -L 2>/dev/null \
         | rg "sysfs/leds/" \
         | rg -i "${1:-${LED_LIGHT}}" \
         | sed "s|\t||g" \
         | while read light
         do
-            light -Srs ${light} ${2:-1}
+            light -Srs ${light} ${2:-1} 2>/dev/null
         done
 }
 
 function led_get() {
-    light -L \
+    light -L 2>/dev/null \
         | rg "sysfs/leds/" \
         | rg -i "${1:-${LED_LIGHT}}" \
         | sed "s|\t||g" \
         | while read light
         do
-            echo "${light} $(light -Grs ${light})"
+            echo "${light} $(light -Grs ${light} 2>/dev/null)"
         done
 }
 
@@ -37,12 +37,12 @@ function led_off() {
 }
 
 function led_toggle() {
-    led_get ${1:-${LED_LIGHT}} \
+    led_get "${1:-${LED_LIGHT}}" \
         | while read light_state
         do
             light=$(echo "${light_state}" | cut -d' ' -f1)
             state=$(echo "${light_state}" | cut -d' ' -f2)
-            light -Srs "${light}" $( if [ ${state} -eq 0 ]; then echo 1 ; else echo 0 ; fi )
+            light -Srs "${light}" "$( if [ "${state}" -eq 0 ]; then echo 1 ; else echo 0 ; fi )" 2>/dev/null
         done
 }
 
@@ -52,7 +52,8 @@ function led_blink() {
     local LED_ON=${3:-${LED_ON}}
     local LED_OFF=${4:-${LED_OFF}}
     local re='^[0-9]+$'
-    if [[ ${LED_REPEAT} =~ ${re} ]] ; then
+    #echo "led_blink: light:${LED_LIGHT}, repeat:${LED_REPEAT}, on:${LED_ON}, off:${LED_OFF}"
+    if [[ "${LED_REPEAT}" =~ ${re} ]] ; then
         (
         for i in $(seq 1 ${LED_REPEAT}); do
             if [ ${i} -eq 1 ]; then
@@ -85,7 +86,8 @@ usage() {
     echo "-l   --list"
     echo "-o   --on"
     echo "-O   --off"
-    echo "-b   --blink"
+    echo "-b   --blink <repeat>"
+    echo "-d   --blink-duration <seconds>"
     echo "-t   --toggle"
 }
 
@@ -105,6 +107,10 @@ while (($#>0)); do
             shift
             BLINK=${1}
             shift;;
+        -d|--blink-duration)
+            shift
+            DURATION=${1}
+            shift;;
         -l|-g|--list|--get)
             LIST=1
             shift;;
@@ -113,6 +119,7 @@ while (($#>0)); do
             shift;;
         -h|--help)
             usage
+            exit
             ;;
         *)
             DEFAULT_LED_LIGHT=${1}
@@ -120,27 +127,17 @@ while (($#>0)); do
     esac
 done
 
-if [ ! -z "${BLINK}" ] || [ ! -z "${DEFAULT_LED_LIGHT}" ]; then
-    led_blink ${DEFAULT_LED_LIGHT:-${LED_LIGHT}} ${BLINK}
-fi
-
-if [ ! -z "${ON}" ]; then
-    led_on ${1}
-fi
-
-if [ ! -z "${OFF}" ]; then
-    led_off ${1}
-fi
-
-if [ ! -z "${TOGGLE}" ]; then
-    led_toggle ${1}
-fi
-
 if [ ! -z "${LIST}" ]; then
     led_get .
-fi
-
-if [ ! -z "${NO_ARGS}" ]; then
+elif [ ! -z "${OFF}" ]; then
+    led_off ${DEFAULT_LED_LIGHT:-${LED_LIGHT}}
+elif [ ! -z "${ON}" ]; then
+    led_on ${DEFAULT_LED_LIGHT:-${LED_LIGHT}}
+elif [ ! -z "${TOGGLE}" ]; then
+    led_toggle ${DEFAULT_LED_LIGHT:-${LED_LIGHT}}
+elif [ ! -z "${BLINK}" ] || [ ! -z "${DEFAULT_LED_LIGHT}" ]; then
+    led_blink ${DEFAULT_LED_LIGHT:-${LED_LIGHT}} "${BLINK}" "${DURATION}" "${DURATION}"
+elif [ ! -z "${NO_ARGS}" ]; then
     led_blink
 fi
 
